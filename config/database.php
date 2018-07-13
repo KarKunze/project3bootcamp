f70e4e60ba6c5507d21a24743c6f7fcd13a6693c
Karla Kunze <karlalkunze@gmail.com>
2018-07-13 13:27:31 -0400

    heroku database connect

--- config/database.php
+++ config/database.php
@@ -56,16 +56,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $dbhost,
+            'port' => $dbport,
+            'database' => $dbname,
+            'username' => $dbuser,
+            'password' => $dbpass,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
